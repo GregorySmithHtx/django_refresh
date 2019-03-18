36af884d337de81702cb34d788aabb763b65e946
Gregory Smith <gregory.terrell.smith@gmail.com>
2019-03-18 09:57:17 -0500

    Update functional test to use Unittest module

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_load_poll(self):
+		self.browser.get('http://localhost:3000')
+		self.assertIn('Polls', self.browser.title)
+		self.fail('Finish Tests')
+
+if __name__ =='__main__':
+	unittest.main(warnings='ignore')
